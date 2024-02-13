00b5fff3724a6942826853f8a20798d8c57c3f31
Bassel Manasrah <bassl.manasrah@gmail.com>
2024-02-13 15:55:45 +0200

    feat(mobile): add loading and disabled states to Button component

--- mobile-app/src/components/Button.js
+++ mobile-app/src/components/Button.js
@@ -1,26 +1,44 @@
-import { Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 
-export default function Button({ children, onPress }) {
+export default function Button({ children, disabled, loading, onPress }) {
+  const containerStyle = [styles.container];
+  if (disabled) containerStyle.push(styles.disabled);
+
   return (
-    <TouchableOpacity onPress={onPress} style={styles.container}>
-      <Text style={styles.text}>{children}</Text>
+    <TouchableOpacity
+      onPress={onPress}
+      disabled={disabled}
+      style={containerStyle}
+    >
+      {loading ? (
+        <ActivityIndicator color="white" size="large" />
+      ) : (
+        <Text style={styles.text}>{children}</Text>
+      )}
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    height: 56,
+    borderRadius: 8,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 8,
-    paddingVertical: 12,
-    paddingHorizontal: 24,
     backgroundColor: "#075eec",
   },
   text: {
     fontSize: 18,
     fontWeight: "bold",
     color: "#fff",
+  },
+  disabled: {
+    opacity: 0.4,
   },
 });
