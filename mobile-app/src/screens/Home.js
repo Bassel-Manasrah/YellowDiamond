2b79a8a53b76ca901b08a9745da67b79abf4003e
Bassel Manasrah <bassl.manasrah@gmail.com>
2024-02-17 16:49:49 +0200

    feat(mobile): add floating action button in home screen to navigate to contacts screen

--- mobile-app/src/screens/Home.js
+++ mobile-app/src/screens/Home.js
@@ -1,11 +1,26 @@
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import FAB from "../components/FAB";
 
-export default function Home() {
+export default function Home({ navigation }) {
+  const onFloatingButtonPress = () => {
+    navigation.navigate("contacts");
+  };
+
+  const floatingButtonIcon = (
+    <Ionicons name="person-add" size={32} color="white" />
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Home</Text>
+      <FAB
+        color="#075eec"
+        icon={floatingButtonIcon}
+        onPress={onFloatingButtonPress}
+      />
     </SafeAreaView>
   );
 }
