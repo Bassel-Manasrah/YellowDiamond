d76bc9ad4da0fc44cac0a018f18982d2973224f4
Bassel Manasrah <bassl.manasrah@gmail.com>
2024-03-03 20:12:06 +0200

    chore(backend): dockerize the registry service

--- apps/registry/src/index.js
+++ apps/registry/src/index.js
@@ -4,6 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
