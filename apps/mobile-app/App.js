80c74926c7b2b1949c9730ed40a20213c4e66d1e
Bassel Manasrah <bassl.manasrah@gmail.com>
2024-03-05 15:46:41 +0200

    feat(mobile): protect the routes that need the user to be signed in

--- apps/mobile-app/App.js
+++ apps/mobile-app/App.js
@@ -6,31 +6,62 @@ import Home from "./src/screens/Home";
 import Contacts from "./src/screens/Contacts";
 import useAppStateHandler from "./src/hooks/useAppStateHandler";
 import Chat from "./src/screens/Chat";
+import { store } from "./src/utils/store";
+import { useEffect, useState } from "react";
+import SplashScreen from "./src/screens/SplashScreen";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export default function App() {
+  const [isLoadingToken, setIsLoadingToken] = useState(true);
+  const [token, setToken] = useState(null);
   const { ready } = useAppStateHandler();
 
+  const fetchToken = async () => {
+    const fetched = await store.get("token");
+    setToken(fetched);
+    setIsLoadingToken(false);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchToken();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  if (isLoadingToken) {
+    return <SplashScreen />;
+  }
+
+  const authScreens = (
+    <>
+      <Screen name="authInit" component={AuthInit} />
+      <Screen name="authVerify" component={AuthVerify} />
+    </>
+  );
+
+  const mainScreens = ready ? (
+    <>
+      <Screen name="home" component={Home} />
+      <Screen
+        name="contacts"
+        component={Contacts}
+        options={{
+          headerShown: true,
+        }}
+      />
+      <Screen name="chat" component={Chat} />
+    </>
+  ) : (
+    <Screen name="splash" component={SplashScreen} />
+  );
+
   return (
-    ready && (
-      <NavigationContainer>
-        <Navigator
-          screenOptions={{ headerShown: false, animation: "slide_from_right" }}
-        >
-          <Screen name="authInit" component={AuthInit} />
-          <Screen name="authVerify" component={AuthVerify} />
-          <Screen name="home" component={Home} />
-          <Screen
-            name="contacts"
-            component={Contacts}
-            options={{
-              headerShown: true,
-            }}
-          />
-          <Screen name="chat" component={Chat} />
-        </Navigator>
-      </NavigationContainer>
-    )
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false, animation: "none" }}>
+        {token ? mainScreens : authScreens}
+      </Navigator>
+    </NavigationContainer>
   );
 }
